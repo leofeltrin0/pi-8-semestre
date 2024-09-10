eaa5896922a33587c5102bf48ce9ae3afd4a2eae
Leonardo Feltrin <leo.moraes123@yahoo.com.br>
2024-09-10 13:25:48 -0300

    port 8080

--- app/stt/api.py
+++ app/stt/api.py
@@ -34,4 +34,4 @@ async def transcribe_and_summarize_endpoint(file: UploadFile = File(...)):
         raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
